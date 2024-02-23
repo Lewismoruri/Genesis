4ffb36867525e1526d4b842e9154c5980e82cfaa
Lewismoruri <morurilewis@gmail.com>
2024-02-23 22:51:58 +0300

    initial commit

---
+++ main.dart
@@ -0,0 +1,4 @@
+// Myfirst dart code
+void main(){
+  print("Hello class, my first dart class");
+}
---
+++ variables.dart
@@ -0,0 +1,23 @@
+void main(){
+  //variables in dart language
+  String school="PLP AFRICA ACADEMY";
+  int total=400;
+  double GPA=4.0;
+  num year=2024;
+  bool marriage_status=true;
+var a="Thursday";
+var A="Wednesday";
+  //lets print our variables
+  print (school);
+  //print ("PLP AFRICA ACADEMY");
+  /*
+  MY DART CODE
+  THIS IS MY JOURNEY IN CODING */
+  print (total);
+  print (GPA);
+  print (year);
+  print (marriage_status);
+  print (a);
+  print (A);
+
+}
